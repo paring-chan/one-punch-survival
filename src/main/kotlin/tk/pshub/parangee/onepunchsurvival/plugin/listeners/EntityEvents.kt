b86c186523c56c417d02d20dbae7c1ce3ef2da6c
parangee9706 <parangee9706@gmail.com>
2020-05-11 14:25:10 +0900

    추가되지 않은 플레이어도 데미지를 받으면 죽는 문제 수정

--- src/main/kotlin/tk/pshub/parangee/onepunchsurvival/plugin/listeners/EntityEvents.kt
+++ src/main/kotlin/tk/pshub/parangee/onepunchsurvival/plugin/listeners/EntityEvents.kt
@@ -1,16 +1,28 @@
 package tk.pshub.parangee.onepunchsurvival.plugin.listeners
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
+import tk.pshub.parangee.onepunchsurvival.plugin.OnePunchSurvival
 
 class EntityEvents:Listener {
+    private val pl = Bukkit.getPluginManager().getPlugin("OnePunchSurvival") as OnePunchSurvival
     @EventHandler
     fun onDamage(e:EntityDamageEvent) {
         if (e.entity is LivingEntity) {
-            var entity = e.entity as LivingEntity
-            entity.health = 0.0
+            if(e.entity is Player) {
+                var player = e.entity as Player
+                if (pl.players.contains(player)) {
+                    var entity = e.entity as LivingEntity
+                    entity.health = 0.0
+                }
+            } else {
+                var entity = e.entity as LivingEntity
+                entity.health = 0.0
+            }
         }
     }
 }
