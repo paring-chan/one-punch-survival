d57fc05fd766ee4748b5d18098b0dcc7620f9ba1
parangee9706 <parangee9706@gmail.com>
2020-05-11 19:34:22 +0900

    활성화시 제작자 메시지 표시

--- src/main/kotlin/tk/pshub/parangee/onepunchsurvival/plugin/OnePunchSurvival.kt
+++ src/main/kotlin/tk/pshub/parangee/onepunchsurvival/plugin/OnePunchSurvival.kt
@@ -1,6 +1,7 @@
 package tk.pshub.parangee.onepunchsurvival.plugin
 
 import com.github.noonmaru.tap.command.command
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import tk.pshub.parangee.onepunchsurvival.plugin.command.CommandAdd
@@ -13,6 +14,9 @@ import tk.pshub.parangee.onepunchsurvival.plugin.listeners.EntityEvents
 class OnePunchSurvival:JavaPlugin() {
     val players = ArrayList<Player>()
     override fun onEnable() {
+        Bukkit.broadcastMessage("원펀맨 야생 플러그인은 파랑이가 제작했습니다.")
+        Bukkit.broadcastMessage("제작자 깃허브: https://github.com/parangee")
+        Bukkit.broadcastMessage("제작자 웹사이트: https://pshub.tk")
         logger.info("원펀맨 야생 시스템 로딩됨")
         setupCommands()
         registerEvents()
